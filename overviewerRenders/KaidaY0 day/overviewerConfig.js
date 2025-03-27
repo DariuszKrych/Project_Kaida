6075be8c520ef692eda36dca6f852cf8de4a4e16
DariuszKrych <darek.krych@gmail.com>
2025-03-27 18:56:32 +0100

    Many changes, adding Y0 map.
    Adding Year 0 isometric map render.
    Many small changes like adding Archer's
    base description, fixing her name being written as Archer instead of ArchedRocketeer in event participation lists and changing the coord box colour on the map renders bottom left to be a lighter shade of blue to increase legibility.

---
+++ overviewerRenders/KaidaY0 day/overviewerConfig.js
@@ -0,0 +1,114 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "compass": {
+                "0": "project_kaida_txt_logo.png",
+                "1": "project_kaida_txt_logo.png",
+                "3": "project_kaida_txt_logo.png",
+                "2": "project_kaida_txt_logo.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Test for Project Kaida"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1742812671",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "Overworld Name",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Test for Project Kaida",
+            "last_rendertime": 1742811745,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                144,
+                48
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                16,
+                144,
+                48
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether Name",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "nether",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Test for Project Kaida",
+            "last_rendertime": 1742811686,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                144,
+                48
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "End Name",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "end",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Test for Project Kaida",
+            "last_rendertime": 1742811885,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                16,
+                144,
+                48
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
